a15b18e2357cecb8202d75ce90400846b68733ca
LHMA <hoangson.hut@gmail.com>
2025-10-27 18:31:54 +0700

    test db

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  console.log(process.env.DB_USER);
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
